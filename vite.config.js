9172d3b24ef62dcb39880466706632ebf33fbbc8
Tim Severien <timseverien@gmail.com>
2021-04-23 13:01:38 +0200

    Add webfonts and move CNAME file

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import postcssPresetEnv from 'postcss-preset-env';
 const DIR_ROOT = __dirname;
 
 export default {
+  publicDir: path.resolve(DIR_ROOT, 'public'),
   root: path.resolve(DIR_ROOT, 'src'),
 
   build: {
