86a06f6386ee07269413908c50487707f5ae6fb0
Tim Severien <timseverien@gmail.com>
2021-04-23 11:23:35 +0200

    WIP

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
 import path from 'path';
+import postcssPresetEnv from 'postcss-preset-env';
 
 const DIR_ROOT = __dirname;
 
 export default {
   root: path.resolve(DIR_ROOT, 'src'),
+
   build: {
     outDir: path.resolve(DIR_ROOT, 'docs'),
+  },
+
+  css: {
+    postcss: {
+      plugins: [
+        postcssPresetEnv({
+          stage: 1,
+        }),
+      ],
+    },
   },
 }
