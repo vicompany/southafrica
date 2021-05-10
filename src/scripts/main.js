91506ff01c782b0e9d6ab671bfb23b6146508c18
Tim Severien <timseverien@gmail.com>
2021-05-10 17:42:30 +0200

    WIP

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
-import './style.css'
+const elementHeader = document.getElementById('header');
+const elementJumbotron = document.getElementById('jumbotron');
+
+const observer = new IntersectionObserver(([state]) => {
+  if (state.isIntersecting) {
+    elementHeader.classList.remove('header--sticky');
+  } else {
+    console.log('make sticky');
+    elementHeader.classList.add('header--sticky');
+    elementHeader.classList.add('header--sticky-animation-start');
+
+    setTimeout(() => {
+      elementHeader.classList.remove('header--sticky-animation-start');
+    }, 100);
+  }
+});
+
+observer.observe(elementJumbotron);
